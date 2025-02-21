ca78ac8f85d6eb90701d67d5d60387d2902eaa01
abbie <alabaabbieclaire@gmail.com>
2025-02-21 17:43:16 +0800

    Activity 2: add new user

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Students;
+use App\Models\User;
+
+class StudentsController extends Controller
+{
+
+    public function myView()
+    {
+        $students = Students::all();
+        $users = User::all();
+
+        return view('welcome', compact('students', 'users'));
+    }
+
+    public function addNewStudent(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'age' => 'required',
+            'gender' => 'required',
+        ]);
+
+        $add_new = new Students;
+        $add_new->id = $request->id;
+        $add_new->name = $request->name;
+        $add_new->age = $request->age;
+        $add_new->gender = $request->gender;
+        $add_new->save();
+
+
+
+        return back()->with('success', 'Student added successfully');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// View
+Route::get('/', [StudentsController::class, 'myView'])->name('std.myView');
+// Create
+Route::post('/add-new', [StudentsController::class, 'addNewStudent'])->name('std.addNewStudent');
